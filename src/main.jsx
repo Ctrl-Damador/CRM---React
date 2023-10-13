23dbf3ea5d816dc623d4a11ce10de01b2b2010c9
David Amador <joseph.amador.segura18@gmail.com>
2023-10-13 16:37:46 -0600

    Interfaz finalizada

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Components/Layout'
+import NuevoCliente from './Pages/NuevoCliente'
+import Index, { loader as ClientesLoader} from './Pages/Index'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+        loader: ClientesLoader 
+      },
+      {
+        path: '/clientes/nuevo',
+        element: <NuevoCliente />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
